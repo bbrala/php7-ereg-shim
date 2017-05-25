2526ed869caeaf5c324af9b37e1d02740ffe4e49
Just another S. Hol <s.s.holwegner@gmail.com>
2017-05-25 09:34:30 -0700

    str_replace()- escape possible issues with delims
    Added trivial inline str_replace() to escape possibly passed delimiters.  This still isn't really a replacement for ereg() functions, but makes trivial use, well, trivial.

--- lib/ereg.php
+++ lib/ereg.php
@@ -3,32 +3,33 @@ namespace {
     if (!function_exists('ereg')) {
         function ereg($pattern, $subject, &$matches = array())
         {
-            return preg_match('/'.$pattern.'/', $subject, $matches);
+            return preg_match('/'.str_replace("/", "\/", $pattern).'/', $subject, $matches);
         }
 
         function eregi($pattern, $subject, &$matches = array())
         {
-            return preg_match('/'.$pattern.'/i', $subject, $matches);
+            return preg_match('/'.str_replace("/", "\/", $pattern).'/i', $subject, $matches);
         }
 
         function ereg_replace($pattern, $replacement, $string)
         {
-            return preg_replace('/'.$pattern.'/', $replacement, $string);
+            return preg_replace('/'.str_replace("/", "\/", $pattern).'/', $replacement, $string);
         }
 
         function eregi_replace($pattern, $replacement, $string)
         {
-            return preg_replace('/'.$pattern.'/i', $replacement, $string);
+            return preg_replace('/'.str_replace("/", "\/", $pattern).'/i', $replacement, $string);
         }
 
         function split($pattern, $subject, $limit = -1)
         {
-            return preg_split('/'.$pattern.'/', $subject, $limit);
+            return preg_split('/'.str_replace("/", "\/", $pattern).'/', $subject, $limit);
         }
 
         function spliti($pattern, $subject, $limit = -1)
         {
-            return preg_split('/'.$pattern.'/i', $subject, $limit);
+            return preg_split('/'.str_replace("/", "\/", $pattern).'/i', $subject, $limit);
         }
     }
 }
+?>
